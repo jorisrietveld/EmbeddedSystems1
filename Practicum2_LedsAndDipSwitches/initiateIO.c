279ddda175f46c05b31125f9ecdb5479e23555d3
Joris Rietveld <jorisrietveld@gmail.com>
2017-09-22 11:26:28 +0200

    Updated the pulse counter.

--- Practicum2_LedsAndDipSwitches/initiateIO.c
+++ Practicum2_LedsAndDipSwitches/initiateIO.c
@@ -11,14 +11,14 @@
  */
 void initiateIO(void)
 {
-    DDRC |= (1<<PC7); // Set port C1 to for output;
-    DDRC |= (1<<PC6); // Set port C2 to for output;
-    DDRC |= (1<<PC5); // Set port C3 to for output;
-    DDRC |= (1<<PC4); // Set port C4 to for output;
-    DDRC |= (1<<PC3); // Set port C5 to for output;
-    DDRC |= (1<<PC2); // Set port C6 to for output;
-    DDRC |= (1<<PC1); // Set port C7 to for output;
-    DDRC |= (1<<PC0); // Set port C8 to for output;
+    DDRC |= ( 1<<7 ); // Set port C1 to for output;
+    DDRC |= ( 1<<6 ); // Set port C2 to for output;
+    DDRC |= ( 1<<5 ); // Set port C3 to for output;
+    DDRC |= ( 1<<4 ); // Set port C4 to for output;
+    DDRC |= ( 1<<3 ); // Set port C5 to for output;
+    DDRC |= ( 1<<2 ); // Set port C6 to for output;
+    DDRC |= ( 1<<1 ); // Set port C7 to for output;
+    DDRC |= ( 1<<0 ); // Set port C8 to for output;
 
     DDRD &= ~(1<<7); 	//Set Pin D1 for input
     PORTD |= (1<<7); //Set internal pull-up resistor (after setting pin for input)
--- Practicum5_PulseCounter/main.c
+++ Practicum5_PulseCounter/main.c
@@ -9,13 +9,31 @@
 
 #include <util/delay.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 volatile static uint8_t counterMode = 0; //  The mode of the pulse counter
+volatile static uint16_t countedPulses = 9995; // The counter
 static uint8_t numberBase = 10; // The base the use when counting.
 
 // Encoded numbers for 7 segment display, where the indexes correspond to the value being displayed. numbers 1 to 16 (hexadecimal) 
-static uint8_t encodedNumbers[16] = { 0x82, 0xbb, 0x85, 0x91, 0x8b, 0xd0, 0xc0, 0x9B, 0x80, 0x90 ,0x88,	0xe0, 0xc6, 0xa1, 0xc4, 0xcc, };
+static uint8_t encodedNumbers[16] = { 0x82, 0xbb, 0x85, 0x91, 0b10111000, 0xd0, 0xc0, 0x9B, 0x80, 0x90 ,0x88,	0xe0, 0xc6, 0xa1, 0xc4, 0xcc, };
 
+/**
+ * This function gets executed when the external interrupt 0 is triggered. It will track the state of the leds
+ * and update there values accordingly. The pattern used:
+ */
+ISR (INT0_vect)
+{
+    countedPulses++; // Increase the pulse count by one.
+	countedPulses %= 10000;
+}
+
+/**
+ * Write encoded number to the display using multiplexing.
+ *
+ * @param segmentByte The encoded number.
+ * @param displayIndex The display to print it on.
+ */
 static void writeSegmentSequence( uint8_t segmentByte, uint8_t displayIndex)
 {
     PORTA = ~( 1 << displayIndex ); // Set the correct display.
@@ -24,11 +42,17 @@ static void writeSegmentSequence( uint8_t segmentByte, uint8_t displayIndex)
         if( !( segmentByte & ( 1 << i ) ) ) // If the bit is 0 at the n'th position of the byte.
         {
             PORTC = ~( 1 << i ); // Turn on the corresponding segment (remember that 0 means on)
-			_delay_us(20); 
+			_delay_us(30); 
         }    
     }
+	PORTC = 0xff;
 }
 
+/**
+ * Displays an number on the seven segment display.
+ *
+ * @param number The number to be printed on the display.
+ */
 static void writeNumbToDisplay(uint16_t number)
 {
 	uint8_t index = 0; // Tracks the display that is being written on. 
@@ -42,13 +66,22 @@ static void writeNumbToDisplay(uint16_t number)
 	}
 }
 
+/**
+ * This is the main
+ * @return
+ */
 int main(void)
 {
     DDRA = DDRC = 0xff; // Initiate ports A and C as output ports.
-	
+	// Enable interrupt 0 in the general interrupt control register.
+	GICR = 1 << INT0;
+	// Set the interrupt sens control bits in the MCU control register to listen for an raising edge event.
+	MCUCR = 1 << ISC01 | 1 << ISC00;
+	// Set enable interrupt to the control register so interrupts are enabled globally.
+	sei();
 	
     while (1) 
     {
-		writeNumbToDisplay(8192);
+		writeNumbToDisplay(countedPulses);
     }
 }
