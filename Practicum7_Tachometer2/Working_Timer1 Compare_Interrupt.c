f3d0ee363ab7d06f5d568e60e5ddb4e357a59efb
Joris Rietveld <jorisrietveld@gmail.com>
2017-10-31 19:49:31 +0100

    Working Timer on hardware mode

---
+++ Practicum7_Tachometer2/Working_Timer1 Compare_Interrupt.c
@@ -0,0 +1,43 @@
+/**
+ * Author: Joris Rietveld <jorisrietveld@gmail.com>
+ * Created: 31-10-2017 18:35
+ * License: GPLv3 - General Public License version 3
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define LED_DATA_DIRECTION DDRD
+#define LED_PORT PORTD
+
+volatile uint8_t count = 0;
+
+// Interrupt Service Routine on Timer 1: compare output compare register A with: Timer 1: Count register
+ISR(TIMER1_COMPA_vect)
+{
+    count++;
+    switch (count){
+        case 7:
+            count = 0;
+            LED_PORT = 0xfe;
+            break;
+        default:
+            LED_PORT >>= 1;
+            break;
+    }
+    LED_PORT ^= (1<<7); // Toggle led
+}
+
+int main(){
+    LED_DATA_DIRECTION = 0xff; // Enable the data direction register of of the led port to outputs and pull up all ports.
+    LED_PORT = 0xfe;
+    TCCR1B |= (1 << WGM12 ); // Configure timer 1 for CTC mode, Clear Timer on Compare.
+	TIMSK |= (1 << OCIE1A ); // Enable interrupts on register OCR1A with Timer 1, Output Compare Interrupt Enable 1 A
+	sei(); // Set Enable Interupts, Enable global interrupts.
+	OCR1A = 15624; // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64,
+    TCCR1B |= (1 << CS10) | (1<<CS11); // Enable Timer1 with an prescaler of 64
+
+    while(1){
+        asm volatile ("nop"); // Do nothing , No Operation
+    }
+}
+
+
--- Practicum7_Tachometer2/main.c
+++ Practicum7_Tachometer2/main.c
@@ -1,43 +1,20 @@
 /**
  * Author: Joris Rietveld <jorisrietveld@gmail.com>
- * Created: 31-10-2017 18:35
+ * Created: 31-10-2017 19:16
  * License: GPLv3 - General Public License version 3
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define LED_DATA_DIRECTION DDRD
-#define LED_PORT PORTD
 
-volatile uint8_t count = 0;
-
-// Interrupt Service Routine on Timer 1: compare output compare register A with: Timer 1: Count register
-ISR(TIMER1_COMPA_vect)
+int main (void)
 {
-    count++;
-    switch (count){
-        case 7:
-            count = 0;
-            LED_PORT = 0xfe;
-            break;
-        default:
-            LED_PORT >>= 1;
-            break;
-    }
-    LED_PORT ^= (1<<7); // Toggle led
-}
-
-int main(){
-    LED_DATA_DIRECTION = 0xff; // Enable the data direction register of of the led port to outputs and pull up all ports.
-    LED_PORT = 0xfe;
-    TCCR1B |= (1 << WGM12 ); // Configure timer 1 for CTC mode, Clear Timer on Compare.
-	TIMSK |= (1 << OCIE1A ); // Enable interrupts on register OCR1A with Timer 1, Output Compare Interrupt Enable 1 A
-	sei(); // Set Enable Interupts, Enable global interrupts.
-	OCR1A = 15624; // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64,
-    TCCR1B |= (1 << CS10) | (1<<CS11); // Enable Timer1 with an prescaler of 64
+    DDRD |= (1 << 5); // Set LED as output
+    TCCR1B |= (1 << WGM12); // Configure timer 1 for CTC mode
+    TCCR1A |= (1 << COM1A0); // Enable timer 1 Compare Output channel A in toggle mode
+    OCR1A   = 15624; // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
+    TCCR1B |= ((1 << CS10) | (1 << CS11)); // Start timer at Fcpu/64
 
     while(1){
         asm volatile ("nop"); // Do nothing , No Operation
     }
 }
-
-
