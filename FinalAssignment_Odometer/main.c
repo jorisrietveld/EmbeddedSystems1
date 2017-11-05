c62d69ec2eab44e335053fac48c8d07e57f3276e
Joris Rietveld <jorisrietveld@gmail.com>
2017-11-05 15:41:52 +0100

    Updated project.

--- FinalAssignment_Odometer/main.c
+++ FinalAssignment_Odometer/main.c
@@ -15,4 +15,54 @@
 #define INPUT_PIN PIND  // Input Register of to the external input ports, used to interact with the user and read data from the sensors.
 #define INPUT_PORT PORTD // Output Register of the external input ports, used to enable pull-up registers.
 
-volatile uint16_t
+volatile uint
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void initIO(); // Declare function for setting up I/O.
+void initTimer(); // Declare function for setting up the timers.
+void initInterrupts(); // Declare function for setting up the interrupts.
+
+/**
+ * The main program routine and initial starting point of the program.
+ */
+int main(){
+    initIO();
+    initTimer();
+    initInterrupts();
+    while(1){
+        asm volatile ("nop"); // Do nothing , No Operation
+    }
+}
+
+/**
+ * This function is for setting up the the I/O registers.
+ */
+void initIO(){
+    DISPLAY_DATA_DIR = 0xff; // Configure the data direction registers for the display ports as outputs.
+    SEGMENT_DATA_DIR = 0xff; // Configure the data direction registers for the segment ports as outputs.
+    DISPLAY_PORT = 0xFF;
+    SEGMENT_PORT = 0xFF;
+}
+
+/**
+ * This function is for setting up the interrupt control registers.
+ */
+void initInterrupts(){
+    TIMSK = 1 << OCIE1A; // Enable interrupt on timer 1, compare register A
+    sei();
+}
+
+/**
+ * This function is for setting up the timer registers.
+ */
+void initTimer(){
+    TCCR1B |= 1<< WGM12; // Enable Timer 1 waveform modes.
+    OCR1A = 15624; // Count to 1 second before triggering an interrupt. (freq / (MCU freq / Prescaler) = (1 / (1 000 000 / 64 ) = 15624
+    TCCR1B |= 1 << CS10 | 1 << CS11; // Enable timer 1 from an internal clock source and prescaler of 64.
+}
+
+ISR(TIMER1_COMPA_vect){
+    DISPLAY_PORT ^= (1<<7); // Toggle led.
+}
