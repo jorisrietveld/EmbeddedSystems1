44049cda392f87fabad4674791fcb5bcd7285e7c
Joris Rietveld <jorisrietveld@gmail.com>
2017-11-05 17:40:19 +0100

    Added timer 0;

--- FinalAssignment_Odometer/main.c
+++ FinalAssignment_Odometer/main.c
@@ -1,8 +1,14 @@
 /**
  * Author: Joris Rietveld <jorisrietveld@gmail.com>
- * Created: 03-11-2017 10:24
+ * Author: Randy Dijskhoorn <radijkshoorn@gmail.com>
+ * Created: 05-11-2017 10:24
  * License: GPLv3 - General Public License version 3
  */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sfr_defs.h>
+
 #define DISPLAY_DATA_DIR DDRA // Data Direction Register of to the display selector ports.
 #define DISPLAY_PIN PINA // Input Register of to the display selector ports.
 #define DISPLAY_PORT PORTA // Output Register of the display selector ports, used to multiplex the segment leds.
@@ -15,11 +21,7 @@
 #define INPUT_PIN PIND  // Input Register of to the external input ports, used to interact with the user and read data from the sensors.
 #define INPUT_PORT PORTD // Output Register of the external input ports, used to enable pull-up registers.
 
-volatile uint
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/sfr_defs.h>
+volatile uint16_t countedPulses = 0; // The amount of pulses
 
 void initIO(); // Declare function for setting up I/O.
 void initTimer(); // Declare function for setting up the timers.
@@ -35,6 +37,7 @@ int main(){
     initInterrupts();
     while(1){
         asm volatile ("nop"); // Do nothing , No Operation
+        checkSwitches();
     }
 }
 
@@ -52,7 +55,8 @@ void initIO(){
  * This function is for setting up the interrupt control registers.
  */
 void initInterrupts(){
-    TIMSK = 1 << OCIE1A; // Enable interrupt on timer 1, compare register A
+    TIMSK |= 1 << OCIE1A; // Enable interrupt on timer 1, compare register A
+    TIMSK |= 1 << TOIE0; // Enable interrupt on timer 0 overflow.
     sei();
 }
 
@@ -60,31 +64,41 @@ void initInterrupts(){
  * This function is for setting up the timer registers.
  */
 void initTimer(){
-    TCCR1B |= 1<< WGM12; // Enable Timer 1 waveform modes.
-    OCR1A = 15624; // Count to 1 second before triggering an interrupt. (freq / (MCU freq / Prescaler) = (1 / (1 000 000 / 64 ) = 15624
+    TCCR1B |= 1<< WGM12; // Enable Timer 1 waveform modes. (CTC mode) clear timer on compare
     TCCR1B |= 1 << CS10 | 1 << CS11; // Enable timer 1 from an internal clock source and prescaler of 64.
-    TCCR0 =
-}
+    OCR1A = 15624; // Count to 1 second before triggering an interrupt. (freq / (MCU freq / Prescaler) = (1 / (1 000 000 / 64 ) = 15624
+    TCCR0 = 1<<CS01; // Enable timer 0 from the internal clock source and a prescaler of 8.
 
-void checkSwithes(){
-    if(bit_is_clear(PIND, 0))
-    {
-        pressed_Confidence_level ++;
-        if(pressed == 0)
-        {
-            PORTD ^= 1 << PIND0;
-            PORTD ^= 1 << PIND2;
-            pressed = 1;
-        }
-        else
-        {
-            Released_confidence_level ++;
-            pressed = 0;
+    // calculate amount of ticks: 1000uS / 8uS = 127 ticks, Calculate initial register value 256 - 127 = 129
+    TCNT0 = 129; // Overflow every 1 millisecond.
+
+}
+uint8_t pressed = 0;
+uint16_t pressedConfidenceLevel = 0;
+
+void checkSwitches(){
+    if(bit_is_clear(PIND, 0) ){
+        pressedConfidenceLevel++;
+        if( pressedConfidenceLevel > 200 ){
+            DISPLAY_PORT ^= (1<<7); // Toggle led.
         }
     }
+    else{
+        pressedConfidenceLevel = 0;
+    }
+
+    if( pressedConfidenceLevel > 1000 ){
+        DISPLAY_PORT ^= (1<<7); // Toggle led.
+    }
+
+
+}
+
+ISR(INT0_vect){
+    countedPulses++;
 }
 
 ISR(TIMER1_COMPA_vect){
-    DISPLAY_PORT ^= (1<<7); // Toggle led.
+    //DISPLAY_PORT ^= (1<<7); // Toggle led.
 }
 
